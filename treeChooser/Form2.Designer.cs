28a6531b55089bc3c9fbe05e0b6e37a8c6ffd3fb
NataKost <114850381+IlyaSalRCA@users.noreply.github.com>
2023-10-18 22:20:36 +0300

    0.4

--- treeChooser/Form2.Designer.cs
+++ treeChooser/Form2.Designer.cs
@@ -31,12 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(175, 58);
+            this.label1.Location = new System.Drawing.Point(12, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 16);
             this.label1.TabIndex = 0;
@@ -45,26 +46,37 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(175, 92);
+            this.label2.Location = new System.Drawing.Point(12, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(33, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Ель";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(175, 126);
+            this.label3.Location = new System.Drawing.Point(12, 126);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(32, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Дуб";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(889, 245);
+            this.ClientSize = new System.Drawing.Size(357, 245);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -80,5 +92,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- treeChooser/Form2.cs
+++ treeChooser/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,8 @@ namespace treeChooser
 {
     public partial class Form2 : Form
     {
+        private int treeTypeIndex = 0;
+
         public Form2(double pineResult, double firResult, double oakResult)
         {
             InitializeComponent();
@@ -19,6 +22,52 @@ namespace treeChooser
             label1.Text = $"Древесина соответствует сосне на {pineResult}%";
             label2.Text = $"Древесина соответствует ели на {firResult}%";
             label3.Text = $"Древесина соответствует дубу на {oakResult}%";
+
+            ChoosingTreeType(pineResult, firResult, oakResult);
+        }
+
+        void ChoosingTreeType(double pineResult, double firResult, double oakResult)
+        {
+            double[] trees = new double[] {pineResult, firResult, oakResult};
+
+            Array.Sort(trees);
+
+            if (trees[trees.Length - 1] > 80)
+            {
+                if(trees[trees.Length - 1] == pineResult)
+                {
+                    button1.Text = "Выбрать сосну";
+                    treeTypeIndex = 1;
+
+                }
+                else if (trees[trees.Length - 1] == firResult)
+                {
+                    button1.Text = "Выбрать ель";
+                    treeTypeIndex = 2;
+                }
+                else
+                {
+                    button1.Text = "Выбрать дуб";
+                    treeTypeIndex = 3;
+                }
+            }
+            else
+            {
+                button1.Width = 235;
+                button1.Location = new Point(18, 141);
+                button1.Text = "Ваше дерево не определено в программе";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch(treeTypeIndex)
+            {
+                case 0:
+                    this.Close();
+                    break;
+            }
         }
     }
 }
